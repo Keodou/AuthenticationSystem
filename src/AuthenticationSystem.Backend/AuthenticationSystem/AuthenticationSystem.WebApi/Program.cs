d30cf172ba6d64ac4c63f9be81fa193edf7e8b6f
Dmitry Korobeyshchikov <korobeyshchikov.d@mail.ru>
2023-09-23 19:15:00 +0300

    Configuring the repository

---
+++ src/AuthenticationSystem.Backend/AuthenticationSystem/AuthenticationSystem.WebApi/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Service container
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
